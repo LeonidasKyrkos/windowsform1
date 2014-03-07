67a00d4c26163d4369007ffab21e535fbbae2a4e
LeonidasKyrkos <leo.kyrkos@gmail.com>
2014-03-07 19:45:50 +0000

    second commit

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windowsforms
+{
+    public partial class LeosFirstForm : Form
+    {
+        public LeosFirstForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string bellend;
+
+            bellend = "A Bellend";           
+
+            button1.Text = bellend;
+
+            button2.Visible = true;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
